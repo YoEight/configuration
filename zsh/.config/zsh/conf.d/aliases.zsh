2885fe13b68d37c852ec3344d5c6c932ef9f3056
Yo Eight <yo.eight@gmail.com>
2026-02-16 13:30:49 -0500

    feat: move to `zpresto` as zsh configuration manager (#1)

--- zsh/.config/zsh/conf.d/aliases.zsh
+++
@@ -1,1 +0,0 @@
-alias ll='ls -G -lah'
--- zsh/.config/zsh/conf.d/meilisearch.zsh
+++
@@ -1,4 +0,0 @@
-export LINDERA_CACHE=$HOME/.cache/meili/lindera
-export MILLI_BENCH_DATASETS_PATH=$HOME/.cache/meili/benches
-export MEILI_TEST_FULL_SNAPS=true
-export MEILI_NO_VERGEN=true
